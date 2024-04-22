f6415c655801fdb2464e76d06c890498fac20531
cfbatista <camilafreitasbatista@gmail.com>
2024-04-22 16:40:42 -0300

    add: home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>HOME</div>
+  <div class="q-pa-md">
+    <q-card-section class="text-center">
+        <div class="text-grey-9 text-h5 text-weight-bold">Bem vindo!</div>
+      </q-card-section>
+    <div class="q-gutter-md row justify-center">
+      <q-btn color="dark"
+        style="border-radius: 8px;"
+        rounded
+        size="md"
+        class="q-mt-md q-mr-sm"
+        no-caps
+        @click="logout">Logout</q-btn>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  // name: 'ComponentName'
+  name: 'HomePage',
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/');
+    }
+  }
 })
 </script>
